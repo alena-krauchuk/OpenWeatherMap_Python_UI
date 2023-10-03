5396bb00434649c89eeb23d7023d89d7a77c38e8
Elena <peregovorun@gmail.com>
2023-10-03 23:53:59 +0300

    feat: add conftest.py

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+
+import allure
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from locators.sign_in_page_locators import SingInPageLocators as sil
+from locators.header_page_locators import HeaderPageLocators as hpl
+from data.data_urls import MAIN_PAGE_URL
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+def driver():
+    print('\nstart browser...')
+    chrome_options = Options()
+    if 'CI' in os.environ:
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        driver = webdriver.Chrome(service=ChromeService(), options=chrome_options)
+        driver.set_window_size(1900, 1000)
+    else:
+        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        driver = webdriver.Chrome(service=ChromeService())
+        driver.maximize_window()
+    yield driver
+    print('\nquit browser...')
+    driver.quit()
+
