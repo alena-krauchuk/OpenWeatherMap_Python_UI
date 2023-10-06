650f65020cd129cc3449e33860322623f3ca0c75
Elena <peregovorun@gmail.com>
2023-10-06 13:34:45 +0300

    feat: ref footer_page.py

--- pages/footer_page.py
+++ pages/footer_page.py
@@ -1,10 +1,10 @@
 """This section contains the basic steps for running footer tests"""
 from pages.base_page import BasePage
-from locators.footer_page_locators import FooterPageLocators
+from locators.footer_locators import FooterLocators
 
 
 class FooterPage(BasePage):
-    footer_locators = FooterPageLocators()
+    footer_locators = FooterLocators()
 
     def check_footer_presence(self):
         """Checks Footer is present in the DOM tree"""
