c65603e36372d199deaff7bbe6c03deebf2ea21b
Elena <peregovorun@gmail.com>
2023-10-06 13:35:05 +0300

    feat: ref test_footer.py

--- tests/test_footer.py
+++ tests/test_footer.py
@@ -1,12 +1,15 @@
 import pytest
 from pages.footer_page import FooterPage
+from locators.footer_locators import FooterLocators
+from data.data_urls import url_main_page
 
 
 class TestFooter:
+    footer_locators = FooterLocators()
 
     def test_tc_01_01_01_check_presence_of_footer_on_pages(self, driver):
         """Checks if the footer is present in the DOM tree"""
-        page = FooterPage(driver)
+        page = FooterPage(driver, url_main_page)
         page.open()
-        footer_section = page.check_footer_presence()
-        assert footer_section, "Footer is not present in the DOM tree"
+        assert page.element_is_visible(self.footer_locators.FOOTER_SECTION)
+
