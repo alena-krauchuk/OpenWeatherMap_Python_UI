22707ef4b360f75964705b6f9af4778de30bbc01
Elena <peregovorun@gmail.com>
2023-10-09 20:25:49 +0300

    feat: ref conftest.py

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-from data.data_urls import url_main_page
+from data.data_urls import URL_MAIN_PAGE
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
@@ -21,12 +21,12 @@ def driver():
     if 'CI' in os.environ:
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-        driver = webdriver.Chrome(service=ChromeService(), options=chrome_options)
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        # driver = webdriver.Chrome(service=ChromeService(), options=chrome_options)
         driver.set_window_size(1900, 1000)
     else:
-        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-        driver = webdriver.Chrome(service=ChromeService())
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        # driver = webdriver.Chrome(service=ChromeService())
         driver.maximize_window()
     yield driver
     print('\nquit browser...')
