80c4482a97431c41cc42d740ee401d96bb9c1eaf
Elena <peregovorun@gmail.com>
2023-10-11 12:08:53 +0300

    feat: ref test_footer.py

--- tests/test_footer.py
+++ tests/test_footer.py
@@ -7,20 +7,26 @@ from data.data_urls import URL_MAIN_PAGE
 class TestFooter:
     footer_locators = FooterLocators()
 
-    def test_tc_01_01_01_check_presence_of_footer(self, driver):
-        """Checks if the footer is present in the DOM tree"""
-        page = FooterPage(driver, URL_MAIN_PAGE)
-        page.open()
-        assert page.element_is_present(self.footer_locators.FOOTER_SECTION)
+    class TestFooterPresence:
+        footer_locators = FooterLocators()
 
-    def test_tc_01_01_02_check_visibility_of_footer(self, driver):
-        """Checks if the footer is visible on the Main Page"""
-        page = FooterPage(driver, URL_MAIN_PAGE)
-        page.open()
-        assert page.element_is_visible(self.footer_locators.FOOTER_SECTION)
+        def test_tc_01_01_01_check_presence_of_footer(self, driver):
+            """Checks if the footer is present in the DOM tree"""
+            page = FooterPage(driver, URL_MAIN_PAGE)
+            page.open()
+            assert page.element_is_present(self.footer_locators.FOOTER_SECTION)
 
-    def test_tc_01_02_01_check_presence_of_product_collections_section(self, driver):
-        """Checks if the Product Collections section is present in the DOM tree"""
-        page = FooterPage(driver, URL_MAIN_PAGE)
-        page.open()
-        assert page.element_is_present(self.footer_locators.PRODUCT_COLLECTIONS_SECTION)
+        def test_tc_01_02_01_check_presence_of_product_collections_section(self, driver):
+            """Checks if the Product Collections section is present in the DOM tree"""
+            page = FooterPage(driver, URL_MAIN_PAGE)
+            page.open()
+            assert page.element_is_present(self.footer_locators.PRODUCT_COLLECTIONS_SECTION)
+
+    class TestFooterVisibility:
+        footer_locators = FooterLocators()
+
+        def test_tc_01_01_02_check_visibility_of_footer(self, driver):
+            """Checks if the footer is visible on the Main Page"""
+            page = FooterPage(driver, URL_MAIN_PAGE)
+            page.open()
+            assert page.element_is_visible(self.footer_locators.FOOTER_SECTION)
