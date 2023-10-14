d7b01191532cb5bfdf4efc84cf03b5c947785821
Elena <peregovorun@gmail.com>
2023-10-14 23:54:53 +0300

    feat: add TC_01.01.04 update test_footer.py, locators.py

--- locators/footer_locators.py
+++ locators/footer_locators.py
@@ -7,4 +7,5 @@ class FooterLocators:
     FOOTER_SECTION = (By.CSS_SELECTOR, "#footer-website")
     PRODUCT_COLLECTIONS_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(1) > div:nth-child(1)")
     SUBSCRIPTION_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(1) > div:nth-child(2)")
+    TECHNOLOGIES_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(2) > div:nth-child(1)")
 
