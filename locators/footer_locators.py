c893bedf25f0d7d5182ebd3d39377bdb9cc73225
Elena <peregovorun@gmail.com>
2023-10-16 00:15:31 +0300

    feat: add TC_01.02.08 update test_footer.py, footer_locators.py

--- locators/footer_locators.py
+++ locators/footer_locators.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 
 class FooterLocators:
     COMPANY_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(1) > div:nth-child(3)")
+    DOWNLOAD_OPENWEATHER_APP_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(3)")
     FOOTER_SECTION = (By.CSS_SELECTOR, "#footer-website")
     PRODUCT_COLLECTIONS_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(1) > div:nth-child(1)")
     SINGLE_LINKS_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(2) > div:nth-child(3)")
--- tests/test_footer.py
+++ tests/test_footer.py
@@ -57,6 +57,13 @@ class TestFooter:
             assert page.element_is_present(self.footer_locators.SINGLE_LINKS_SECTION), \
                 "The single links section is not present in the DOM tree"
 
+        def test_tc_01_01_08_check_presence_of_download_openweather_app_section(self, driver):
+            """Checks if the Download OpenWeather app section is present in the DOM tree"""
+            page = FooterPage(driver, URL_MAIN_PAGE)
+            page.open()
+            assert page.element_is_present(self.footer_locators.DOWNLOAD_OPENWEATHER_APP_SECTION), \
+                "The Download OpenWeather app section is not present in the DOM tree"
+
     class TestFooterVisibility:
         footer_locators = FooterLocators()
 
