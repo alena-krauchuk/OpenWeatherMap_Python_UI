fea7a9f9485d8e8839092406167b13e2895b60d6
Elena <peregovorun@gmail.com>
2023-10-23 18:44:45 +0300

    feat: ref TC_01.01.23 update test_footer.py, footer_locators.py

--- locators/footer_locators.py
+++ locators/footer_locators.py
@@ -26,6 +26,7 @@ class FooterLocators:
     SUBSCRIPTION_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(1) > div:nth-child(2)")
     SUBSCRIPTION_SECTION_TITLE = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(1) > div:nth-child(2) p")
     TECHNOLOGIES_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(2) > div:nth-child(1)")
+    TECHNOLOGIES_SECTION_TITLE = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(2) > div:nth-child(1) p")
     TERMS_AND_CONDITIONS_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(2) > div:nth-child(2)")
     WEATHER_DASHBOARD_LINK = (By.CSS_SELECTOR, "#footer-website div :nth-child(1) :nth-child(1) ul :nth-child(4) > a")
     WEATHER_MAPS_LINK = (By.CSS_SELECTOR, "#footer-website div :nth-child(1) :nth-child(1) ul :nth-child(3) > a")
