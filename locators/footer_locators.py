1b1d5692197e023ad49d3dd5ca2f81d1c2ca271d
Elena <peregovorun@gmail.com>
2023-11-01 17:20:11 +0300

    feat: add TC_01.01.40 update test_footer.py, footer_locators.py

--- locators/footer_locators.py
+++ locators/footer_locators.py
@@ -39,6 +39,8 @@ class FooterLocators:
                                          "#footer-website > div > div:nth-child(1) > div:nth-child(1) p")
     RMETS_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(3) > div:nth-child(2)")
     RMETS_SECTION_TITLE = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(3) > div:nth-child(2) p")
+    RMETS_ELEMENT = (By.CSS_SELECTOR,
+                     "#footer-website > div > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) :nth-child(2)")
     SINGLE_LINKS_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(2) > div:nth-child(3)")
     SOCIAL_MEDIA_SECTION = (By.CSS_SELECTOR, "#footer-website > div > div:nth-child(4) > div:nth-child(2)")
     SUBSCRIBE_FOR_FREE_LINK = (By.CSS_SELECTOR,
