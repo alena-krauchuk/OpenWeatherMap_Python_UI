4dbf9a6d0c55e747375e887ab43d3b4cc57def93
Elena <peregovorun@gmail.com>
2023-11-06 09:56:14 +0300

    feat: add TC_01.01.51 update test_footer.pyб, footer_locators.py

--- tests/test_footer.py
+++ tests/test_footer.py
@@ -394,6 +394,13 @@ class TestFooter:
                 assert page.element_is_present(self.footer_locators.COOKIES_PANEL_DESCRIPTION), \
                     "The cookies panel description is not present in the DOM tree"
 
+            def test_tc_01_01_51_check_presence_of_cookies_panel_button_allow(self, driver):
+                """Checks if the button Allow all on cookies panel is present in the DOM tree"""
+                page = FooterPage(driver, URL_MAIN_PAGE)
+                page.open()
+                assert page.element_is_present(self.footer_locators.COOKIES_PANEL_BUTTON_ALLOW), \
+                    "The button Allow all on cookies panel is not present in the DOM tree"
+
     class TestFooterVisibility:
         footer_locators = FooterLocators()
 
