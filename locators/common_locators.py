9ad489968883e029f83e57978b00a5f129b8370b
Elena <peregovorun@gmail.com>
2023-11-08 10:27:25 +0300

    feat: add TC_02.02.04 ref test_common_elements.py, common_locators.py

--- locators/common_locators.py
+++ locators/common_locators.py
@@ -7,4 +7,4 @@ class CookiesPanelLocators:
     ALLOW_ALL_BUTTON = (By.CSS_SELECTOR, "button.stick-footer-panel__link")
     COOKIES_PANEL = (By.CSS_SELECTOR, ".stick-footer-panel")
     COOKIES_PANEL_DESCRIPTION = (By.CSS_SELECTOR, ".stick-footer-panel p")
-    MANAGE_COOKIES_LINK = (By.CSS_SELECTOR, "button.stick-footer-panel__link")
+    MANAGE_COOKIES_LINK = (By.CSS_SELECTOR, "a.stick-footer-panel__link")
