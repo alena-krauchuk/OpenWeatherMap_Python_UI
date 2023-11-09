339b868ba38ec82bfe01409a4e2509d98fddfa67
Elena <peregovorun@gmail.com>
2023-11-09 16:17:26 +0300

    feat: add TC_01.03.03, ref footer_data.py, ref test_footer.py

--- data/footer_data.py
+++ data/footer_data.py
@@ -2,5 +2,6 @@
 
 
 class FooterElementsText:
-    PRODUCT_COLLECTIONS_SECTION_TITLE_TEXT = "Product Collections"
     CURRENT_AND_FORECAST_APIS_LINK_TEXT = "Current and Forecast APIs"
+    HISTORICAL_WEATHER_DATA_LINK_TEXT = "Historical Weather Data"
+    PRODUCT_COLLECTIONS_SECTION_TITLE_TEXT = "Product Collections"
